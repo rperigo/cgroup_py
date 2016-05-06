316ee77a500075326342aa79452ad704f6410ce0
Ray Perigo <rperigo@iu.edu>
2016-05-06 12:44:25 -0400

    Bugfix for OOM mailer on unclean shutdown.

--- cgOOMailer.py
+++ cgOOMailer.py
@@ -9,26 +9,26 @@ def eventfd(init_val, flags):
     return libc.eventfd(init_val, flags)
 
 def main ():
-    run = True
-    efd = eventfd(0,0)
-    cge_c = os.open(sys.argv[1], os.O_WRONLY)
-    cgo_c = os.open(sys.argv[2], os.O_RDONLY)
-    uid = sys.argv[3]
-    getU = subprocess.Popen(['getent','passwd',uid], stdout=subprocess.PIPE)
-    uname = getU.communicate()[0].split(':')[0]
-    wb = str(efd)+" "+str(cgo_c)
-    os.write(cge_c, wb)
-    os.close(cge_c)
+	run = True
+	efd = eventfd(0,0)
+	cge_c = os.open(sys.argv[1], os.O_WRONLY)
+	cgo_c = os.open(sys.argv[2], os.O_RDONLY)
+	uid = sys.argv[3]
+	getU = subprocess.Popen(['getent','passwd',uid], stdout=subprocess.PIPE)
+	uname = getU.communicate()[0].split(':')[0]
+	wb = str(efd)+" "+str(cgo_c)
+	os.write(cge_c, wb)
+	os.close(cge_c)
 
-    mailtext = open('/etc/cgroup_OOMemail.txt', 'rb')
-    message = MIMEText(mailtext.read())
-    mailtext.close()
-#  msg = email.MIMEText(msg, plain)
-    fAddress = "sciapt@iu.edu"
-    toAddress = uname+'@iu.edu'
-    message['Subject'] = "Out Of Memory Notification, Karst Desktop BETA"
-    message['From'] = fAddress
-    message['To'] = toAddress
+	mailtext = open('/etc/cgroup_OOMemail.txt', 'rb')
+	message = MIMEText(mailtext.read())
+	mailtext.close()
+	#  msg = email.MIMEText(msg, plain)
+	fAddress = "sciapt@iu.edu"
+	toAddress = uname+'@iu.edu'
+	message['Subject'] = "Out Of Memory Notification, Karst Desktop BETA"
+	message['From'] = fAddress
+	message['To'] = toAddress
     
     
 #     subject = "Karst Desktop Beta Memory Notification"
@@ -42,9 +42,8 @@ def main ():
     
 	while os.path.exists(sys.argv[1]):
 #    while run == True:
-        if len(os.read(efd, 8)) != 8
-            sys.exit(2)
-        ms = smtplib.SMTP('localhost', timeout=30)
-        ms.sendmail(fAddress, toAddress, message.as_string())
-        ms.quit()
+		os.read(efd, 8)
+		ms = smtplib.SMTP('localhost', timeout=30)
+		ms.sendmail(fAddress, toAddress, message.as_string())
+		ms.quit()
 main()
