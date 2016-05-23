84697a4af49d4fd6f7ce1f4a6b92b0c8c1d9e313
Ray Perigo <rperigo@indianaedu>
2016-05-23 14:17:15 -0400

    Updated cleanup script to ensure OOM mailer daemon is scrubbed

--- cgroupClean.sh
+++ cgroupClean.sh
@@ -9,9 +9,19 @@
 # notify_on_release was not able to call the script correctly as empty
 # cgroups would not get removed.
 
-CONFIGFILE=/home/raymond/CGROUP/cgroup_py.cfg
+CONFIGFILE=/etc/cgroup_py.cfg
 CGROUPROOT=$(grep cGroupRoot $CONFIGFILE | cut -d = -f 2 | tr -d '[[:space:]]')
 DSTAMP=$(date +%m/%d/%Y\ %H:%M:%S)
 
+#trim off chars to get numerical UID
+uid=$( echo $1 | tr -d '/' | tr -d [A-z][a-z])
+
+#use that to get this user's OOM mailer process and kill it. This is
+#necessary since just removing their Cgroup directory can cause
+#unintended OOM warning emails.
+getOOMProc=$( ps aux |grep '[/bin/cg]OOM' | grep 501 | tr -s ' ' |cut -d ' ' -f 2 )
+kill -15 $getOOMProc
+
+#now that we've killed their mailer and nobody's watching their eventFD...
 rmdir $CGROUPROOT$1
 echo "$DSTAMP User $1 no longer has running processes. Removing CGroup." >> /var/log/cgroup_py.log
