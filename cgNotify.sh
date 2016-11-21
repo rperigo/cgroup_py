a316b910e43ab6c00e1aebbfa554031bc46c2341
rperigo <rperigo@indiana.edu>
2016-11-21 14:25:33 -0500

    Delete cgNotify.sh

--- cgNotify.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-dbus=$( grep -z DBUS_SESSION_BUS_ADDRESS /proc/$2/environ )
-user=$( getent passwd $3 | cut -d : -f 1 )
-
-export $dbus
-su -c "/usr/bin/notify-send -t 100000 \"$1\"" $user
